32c8de47d44551aef565ac18ccf4eb36d85da9b3
mutsuki <mutsuki@momo86.net>
2021-05-28 15:48:50 +0900

    Add fpz/detail.hpp

---
+++ include/fpz/detail.hpp
@@ -0,0 +1,26 @@
+#ifndef __FPZ_DETAIL_HPP__
+#define __FPZ_DETAIL_HPP__
+#include <cstdint>
+namespace fpz {
+using byte_t = std::uint8_t;
+
+template <class T>
+constexpr unsigned get_stream_block_size();
+
+template <class T>
+void decompose(
+		byte_t* const dst_com_ptr,
+		byte_t* const dst_raw_ptr,
+		const T* const src_ptr,
+		const unsigned num_stream_block
+		);
+
+template <class T>
+void compose(
+		T* const src_ptr,
+		const byte_t* const dst_com_ptr,
+		const byte_t* const dst_raw_ptr,
+		const unsigned num_stream_block
+		);
+} // namespace fpz
+#endif
