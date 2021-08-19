f37d0228d701d1463d3785dcbea6c87c0fc2f9fd
mutsuki <mutsuki@momo86.net>
2021-08-19 20:05:36 +0900

    Update to use `get_stream_com_blocks_bytes` and `get_stream_raw_blocks_bytes` in test

--- test/compress_decompress.cpp
+++ test/compress_decompress.cpp
@@ -15,8 +15,8 @@ void test() {
 	std::printf("### TEST for %s ###\n", get_type_name_str<T>());
 	std::vector<T> input_array(N);
 	std::vector<T> output_array(N);
-	std::vector<typename fpz::byte_t> com_array(fpz::get_com_byte<T>() * fpz::get_num_stream_blocks<T>(N));
-	std::vector<typename fpz::byte_t> raw_array(fpz::get_raw_byte<T>() * fpz::get_num_stream_blocks<T>(N));
+	std::vector<typename fpz::byte_t> com_array(fpz::get_stream_com_blocks_bytes<T>(N));
+	std::vector<typename fpz::byte_t> raw_array(fpz::get_stream_raw_blocks_bytes<T>(N));
 
 	std::mt19937 mt(0);
 	std::uniform_real_distribution<T> mantissa_dist(1, 2);
@@ -41,16 +41,16 @@ void test() {
 		);
 
 	// deflate
-	auto [compressed_data, compressed_size] = fpz::deflate(com_array.data(), fpz::get_com_byte<T>() * fpz::get_num_stream_blocks<T>(N));
-	const auto uncompressed_size = fpz::get_com_byte<T>() * fpz::get_num_stream_blocks<T>(N);
-	const auto raw_part_size = fpz::get_raw_byte<T>() * fpz::get_num_stream_blocks<T>(N);
+	auto [compressed_data, compressed_size] = fpz::deflate(com_array.data(), fpz::get_stream_com_blocks_bytes<T>(N));
+	const auto uncompressed_size = fpz::get_stream_com_blocks_bytes<T>(N);
+	const auto raw_part_size = fpz::get_stream_raw_blocks_bytes<T>(N);
 	std::printf("Exponent compression rate : %luByte -> %luByte (%7.3f)\n", uncompressed_size, compressed_size, 100. * compressed_size / uncompressed_size);
 	std::printf("Total compression rate    : %luByte -> %luByte (%7.3f)\n", uncompressed_size + raw_part_size,
 				compressed_size + raw_part_size,
 				100. * (compressed_size + raw_part_size) / (uncompressed_size + raw_part_size));
 
 	// inflate
-	fpz::inflate(com_array.data(), fpz::get_com_byte<T>() * fpz::get_num_stream_blocks<T>(N), std::make_pair(std::move(compressed_data), compressed_size));
+	fpz::inflate(com_array.data(), fpz::get_stream_com_blocks_bytes<T>(N), std::make_pair(std::move(compressed_data), compressed_size));
 
 	// compose
 	fpz::compose(
