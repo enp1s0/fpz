ed5e2d491b5ee6f3fbc8479b60d897d4ea674288
mutsuki <mutsuki@momo86.net>
2021-08-19 19:47:31 +0900

    Add `fpz::get_num_stream_blocks`

--- include/fpz/detail.hpp
+++ include/fpz/detail.hpp
@@ -13,6 +13,11 @@ template <class T>
 constexpr unsigned get_raw_byte();
 
 template <class T>
+unsigned get_num_stream_blocks(const unsigned num_elements) {
+	return (num_elements + get_stream_block_size<T>() - 1) / get_stream_block_size<T>();
+}
+
+template <class T>
 void decompose(
 		byte_t* const dst_com_ptr,
 		byte_t* const dst_raw_ptr,
